1458a1a1bb5e5b343d6dbb03eb82a4483ec8ab8b
Jack Banks <jackb4@illinois.edu>
2023-02-22 17:37:06 -0600

    NEEDS WORK

---
+++ R/team-total-snaps.R
@@ -0,0 +1,9 @@
+
+# team snap totals
+
+# TODO: load in team blocking, passing, rushing ratings
+# TODO: find each team's total passing and rushing plays from pbp data
+# TODO: join together to find per snap ratings
+
+
+# side note we will look at scrambles separately
